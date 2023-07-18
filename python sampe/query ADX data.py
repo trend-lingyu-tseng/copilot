bd70a8da1d29c12fc93b76f556bd2b381f5ead20
trend-lingyu-tseng <102507512+trend-lingyu-tseng@users.noreply.github.com>
2023-07-18 17:11:23 +0800

    Update query ADX data.py

--- python sampe/query ADX data.py
+++ python sampe/query ADX data.py
@@ -1,6 +1,5 @@
 #this is sample for query adx data
-from pyspark.sql import SparkSession
-pyKusto = SparkSession.builder.appName("kustoPySpark").getOrCreate()
+
 def ADXquery(cluster,db,query,appid,appkey):
   kustoOptions = {"kustoCluster":"private-dataview.westus2", "kustoDatabase" : "VisionOne_Report", "kustoAadAppId":appid ,
  "kustoAadAppSecret":dbutils.secrets.get(scope = "adx-connect", key = appkey), "kustoAadAuthorityID":"trendid"}
