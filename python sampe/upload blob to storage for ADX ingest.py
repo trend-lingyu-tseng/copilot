8d8e28e3bbb740b870572bec94fa302d78b4b505
trend-lingyu-tseng <102507512+trend-lingyu-tseng@users.noreply.github.com>
2023-07-18 08:24:17 +0000

    committed

---
+++ python sampe/upload blob to storage for ADX ingest.py
@@ -0,0 +1,21 @@
+# update blob to storage for ADX ingest
+import io;
+import datetime;
+import time;
+import json;
+import six;
+import os, uuid;
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+import pytz;
+import asyncio;
+from azure.storage.blob.aio import BlobClient;
+
+
+connectsring="your connection string";
+container=ContainerClient.from_container_url(connectsring);
+blob_client =container.get_blob_client('folder/filename');
+
+blob_metadata = {'kustoTable': 'yourtablename', 'kustoDataFormat':'yourdatatype(json/mutijson/csv/..)','kustoIngestionMappingReference':'yourmappingname','kustoDatabase':'yourdatabasename','rawSizeBytes':'filesize(b)'}
+    with open("filename", "rb") as data:
+         blob_client.upload_blob(data)
+         blob_client.set_blob_metadata(metadata=blob_metadata)
